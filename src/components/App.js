8d8f30ead562dbe38c9a8fc2db6fa7467d97be9b
Yipin <xiong1pin@gmail.com>
2019-03-30 23:50:41 +1100

    init project with bootstrap

---
+++ src/components/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import Header from './Header/Header';
+import history from '../history';
+import { Router, Route, Switch } from 'react-router-dom';
+
+const App = ()=>{
+  return (
+    <>
+    <Router history={history}>
+      <div>
+        <Header />
+        <Switch>
+          {/* <Route path="/" exact component={StreamList} />
+          <Route path="/streams/new" exact component={StreamCreate} />
+          <Route path="/streams/edit/:id" exact component={StreamEdit} />
+          <Route path="/streams/delete/:id" exact component={StreamDelete} />
+          <Route path="/streams/:id" exact component={StreamShow} /> */}
+        </Switch>
+      </div>
+    </Router>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+  
+  render() {
+    return (
+      <header className="navbar navbar-dark bg-dark flex-md-nowrap p-2 shadow">
+        <div className="navbar-brand col-sm-3 col-md-2 mr-0">AWARE</div>
+        <ul className="navbar-nav px-3">
+          <li className="nav-item text-nowrap">
+            <div className="nav-link">Sign out</div>
+          </li>
+        </ul>
+      </header>
+      );
+  }
+}
+
+export default Header;
