e24fd2e5596865a82c5f0f4f6539a14880f0b680
Yipin <xiong1pin@gmail.com>
2019-04-15 15:47:52 +1000

    totally screen time diagram

--- django-template/mobile-usage-time/draw.js
+++ django-template/mobile-usage-time/draw.js
@@ -1,26 +1,47 @@
-const width = 900;
-const height =600;
-const padding = 30;
+const width = 600;
+const height =400;
+const padding = 50;
 
 const svg = d3.select('svg')
                 .attr("width", width)
                 .attr("height", height);
 
-// const xScale = d3.scaleLinear()
-//                 .domain(screenUsageData.map(d => d.date))
-//                 .range([padding, width-padding]);
+const xScale = d3.scaleBand()
+                .paddingInner(0.3)
+                .paddingOuter(0.2)
+                .domain(screenUsageData.map(d => d.date))
+                .range([padding, width-padding]);
 
-// const yScale = d3.scaleLinear()
-//                 .domain([0, d3.max(screenUsageData, d => d.duration)])
-//                 .range([height - padding, padding]);
+const yScale = d3.scaleLinear()
+                .domain([0, d3.max(screenUsageData, d => d.duration)])
+                .range([height - padding, padding]);
 
-// const xAxis = d3.axisBottom(xScale)
-//                 .tickSize(-height + 2* padding)
-//                 .tickSizeOuter(0);
+const xAxis = d3.axisBottom(xScale);
 
-// const yAxis = d3.axisBottom(yScale)
-//                 .tickSize(-width + 2* padding)
-//                 .tickSizeOuter(0);
+const yAxis = d3.axisLeft(yScale)
+                  .ticks(4);
+
+
+svg.append("g")
+  .attr('transform', `translate(0,${height-padding})`)
+  .call(xAxis)
+  .selectAll("text")
+    .style("text-anchor", "end")
+    .attr("dx", "-.8em")
+    .attr("dy", "-.55em")
+    .attr("transform", "rotate(-60)" );
+
+svg.append('g')
+    .attr('transform', `translate(${padding}, 0)`)
+    .call(yAxis)
+
+svg.append("text")
+.attr("transform", "rotate(-90)")
+.attr("y", 0 - padding)
+.attr("x",0 - (height / 2))
+.attr("dy", "1em")
+.style("text-anchor", "middle")
+.text("Screen usage hours");    
 
 svg.append("g")
   .selectAll("rect")
@@ -28,7 +49,10 @@ svg.append("g")
     .enter()
     .append("rect")
     .classed("bar", true)
-    .attr("x", (d,i) => i*40 + padding)
-    .attr("width", 30)
-    .attr("y", d=> height - padding - d.duration * 30)
-    .attr("height", d => height - padding)
+    .attr("x", d => xScale(d.date))
+    .attr("width", xScale.bandwidth())
+    .attr("y", d => yScale(d.duration))
+    .attr("height", d => height - padding - yScale(d.duration))
+    .attr("fill", "steelblue");
+    
+    
